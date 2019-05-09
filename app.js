c2ce347d87c3bb65544d2a88de8becfdae3735c8
dejanvajagic <49078368+dejanvajagic@users.noreply.github.com>
2019-05-09 05:49:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -232,10 +232,11 @@ function likedItemsIndexing (callback) {
 
 function likedItems() {
   userlikeditems = localStorage.getItem("user-liked".toString());
-  numHearts = parseInt(localStorage.getItem("numHeartsSaved".toString()), 10);
+  
   var all = document.querySelectorAll('.like_button');
   for (var i = 0; i <= all.length; i++) {
     var loadPage = localStorage.getItem(""+[i].toString());
+    numHearts = parseInt(localStorage.getItem("numHeartsSaved".toString()), 10);
     $("#srce--ikonica").text(numHearts);
     if(numHearts !== 0){
     $("#srce--ikonica").css("color", "white");
