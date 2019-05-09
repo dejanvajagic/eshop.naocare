acbdf93bece8d81cefb4b919a98b8a51ec4a8ad8
dejanvajagic <49078368+dejanvajagic@users.noreply.github.com>
2019-05-09 04:28:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -218,6 +218,8 @@ $('.show-cart').on("change", ".item-count", function(event) {
 
 displayCart();
 
+
+
 // localStorage Cache Code
 function likedItemsIndexing (callback) {
   setTimeout(function() {
@@ -227,10 +229,39 @@ function likedItemsIndexing (callback) {
     callback();
   }, 500);
 }
+
 function likedItems() {
+  userlikeditems = localStorage.getItem("user-liked".toString());
+  numHearts = parseInt(localStorage.getItem("numHeartsSaved".toString()), 10);
+
+
+      $.ajax({
+          url: "save.php?action=save",
+          method: "POST",
+          data: { elem: {
+              poseta: userlikeditems
+          }},
+          success: function (data){
+            alert("Saved!");
+      }
+  });
+
+
+
   var all = document.querySelectorAll('.like_button');
   for (var i = 0; i <= all.length; i++) {
     var loadPage = localStorage.getItem(""+[i].toString());
+    $("#srce--ikonica").text(numHearts);
+    if(numHearts !== 0){
+    $("#srce--ikonica").css("color", "white");
+    }
+
+    // $.getJSON("poseta.json", function(data) {
+    //     alert(data);// Now use this data to update your view models,
+    //     // and Knockout will update your UI automatically
+    // });
+
+
     if (loadPage == null) {
     $("#id-"+[i].toString()+"").html('<i class="far fa-heart like"></i>');
     } else {
@@ -243,18 +274,64 @@ function likedItems() {
     var $unlike = '<i class="far fa-heart like"></i>';
     $(this).html($unlike);
 
+    numHearts--;
+    $("#srce--ikonica").text(numHearts);
+    if(numHearts == 0){
+    $("#srce--ikonica").css("color", "black");
+    }
+
     var copy2 =  $(this).html();
     var replacer2 = $(this).attr("id");
     replacer2 = replacer2.replace('id-','');
     localStorage.setItem(replacer2, copy2);
+    localStorage.setItem("numHeartsSaved", numHearts.toString());
 
   }else if($(this).html() == '<i class="far fa-heart like"></i>'){
   var $likes = '<i class="fas fa-heart like"></i>';
   $(this).html($likes);
+  numHearts++;
+  $("#srce--ikonica").text(numHearts);
+  if(numHearts !== 0){
+  $("#srce--ikonica").css("color", "white");
+  }
+  userlikeditems = ""+userlikeditems+" "+$(this).siblings('.card-title').html()+", ";
+  localStorage.setItem("user-liked", userlikeditems);
+
+  localStorage.setItem("statistika", userlikeditems);
 
   var copy =  $(this).html();
   var replacer = $(this).attr("id");
   replacer = replacer.replace('id-','');
   localStorage.setItem(replacer, copy);
+  localStorage.setItem("numHeartsSaved", numHearts.toString());
 }
 });
+
+
+//Find Hearts
+var numHearts = 0;
+var userlikeditems;
+var n = -1;
+function findHearts(e) {
+    if (numHearts==0){alert("Još nemate favorita? | Dodajte (kliknite) srca kod artikala koji vam se svidjaju, pa kliknite ovde za pregled favorita, oni će vas čekati sledeći put kad posetite stranicu, označeni i spremni za kupovinu!");}
+    else{
+     // window.find("MUŠKE INVU NAOČARE");
+     e.preventDefault();
+     n++;
+     $(window).scrollTop($(".fas:eq("+n+")").offset().top-450);
+ }
+}
+if($("#srce--ikonica").html() == "0"){
+$("#srce--ikonica").css("color", "black");
+}
+
+// mailer code
+// var service_id = 'gmail';
+// var template_id = 'template_eXYdWW2P';
+// var template_params = {
+// 	name: 'John',
+// 	reply_email: 'dejanmail@yahoo.com',
+// 	message: 'This is awesome!'+userlikeditems+""
+// };
+//
+// emailjs.send(service_id,template_id,template_params);
